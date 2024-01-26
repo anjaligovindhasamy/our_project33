9c2640938b244b54d7f144ac0d84d4b96429a0e1
anjaligovindhasamy <anjaligovindhasamy>
2024-01-26 09:47:30 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from app.forms import *
 from django.core.mail import send_mail
+from django.urls import reverse
+from django.contrib.auth import authenticate,login
 
 
 # Create your views here.
@@ -26,8 +28,31 @@ def registration(request):
                        'anjalig1710@gmail.com',
                        [MUFDO.email],
                        fail_silently=False)
-            return HttpResponse('registration is done')
+            return HttpResponse('registration is successfully')
         else:
             return HttpResponse('registration is not done')
         
     return render(request,'registration.html',d)    
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+
+    return render(request,'user_login.html')
